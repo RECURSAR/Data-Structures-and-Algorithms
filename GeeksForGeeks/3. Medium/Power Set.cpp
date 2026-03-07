049d95088a576370a5125c799bcb174b3b00403d
Aditya <90381507+RECURSAR@users.noreply.github.com>
2026-03-07 18:58:47 +0530

    7 MARCH 2026

--- GeeksForGeeks/3. Medium/Power Set.cpp
+++ GeeksForGeeks/3. Medium/Power Set.cpp
@@ -3,8 +3,24 @@
  
 using namespace std;
 
+void generateStrings(string &s, int index, string current, vector<string> &result) {
+    if(index == (int)s.size()) {
+        if(!current.empty())
+            result.push_back(current);
+        return;
+    }
+
+    generateStrings(s, index + 1, current, result);
+    generateStrings(s, index + 1, current + s[index], result);
+}
+
 vector<string> AllPossibleStrings(string s) {
-    
+    vector<string> result;
+
+    generateStrings(s, 0, "", result);
+    sort(result.begin(), result.end());
+
+    return result;
 }
 
 int main() {
